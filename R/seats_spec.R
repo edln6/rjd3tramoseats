19e4cae6534fa21eaba4ff5f83c57c6e75267f3c
palatej <palatejean@gmail.com>
2026-05-06 23:24:06 +0200

    Refactoring

--- R/seats_spec.R
+++ R/seats_spec.R
@@ -58,7 +58,7 @@
 #' (negative values). Default `bcasts` = 0. Default `fcasts` = 0.
 #' @param bias TODO.
 #'
-#' @examplesIf rjd3toolkit::get_java_version() >= rjd3toolkit::minimal_java_version
+#' @examplesIf rjd3jars::check_java_version()
 #' init_spec <- tramoseats_spec("rsafull")
 #' new_spec <- set_seats(init_spec,
 #'     approximation = "Legacy",
--- R/zzz.R
+++ R/zzz.R
@@ -1,31 +1,60 @@
 #' @include utils.R
 NULL
 
-#' @importFrom rjd3toolkit get_java_version minimal_java_version
-.onAttach <- function(libname, pkgname) {
-    current_java_version <- rjd3toolkit::get_java_version()
-    if (current_java_version < rjd3toolkit::minimal_java_version) {
-        packageStartupMessage(sprintf("Your java version is %s. %s or higher is needed.",
-                                      current_java_version, rjd3toolkit::minimal_java_version))
-    }
-}
+#' @importFrom rjd3jars check_java_version
 
 #' @importFrom stats is.ts start
 #' @importFrom RProtoBuf read readProtoFiles2
 #' @importFrom rJava .jpackage .jcall .jnull is.jnull .jfield
+#'
 .onLoad <- function(libname, pkgname) {
     result <- rJava::.jpackage(pkgname, lib.loc = libname)
-    if (!result) stop("Loading java packages failed", call. = FALSE)
+    if (!result)
+        stop("Loading java packages failed", call. = FALSE)
 
-    current_java_version <- rjd3toolkit::get_java_version()
-    if (current_java_version >= rjd3toolkit::minimal_java_version) {
-        rjd3toolkit::reload_dictionaries()
+    if (rjd3jars::check_java_version(FALSE)) {
+        rjd3jars::reload_dictionaries()
     }
 
     proto.dir <- system.file("proto", package = pkgname)
     RProtoBuf::readProtoFiles2(protoPath = proto.dir)
 
+    assign("tramoseats", list(), rjd3toolkit::.jd3_env)
+
     if (is.null(getOption("summary_info"))) {
         options(summary_info = TRUE)
     }
 }
+
+#' Set an option for tramoseats
+#'
+#' @param name Name of the option
+#' @param obj Option
+#'
+#' @export
+#'
+#' @examples
+#' tramoseats_option("test", "DUMMY")
+tramoseats_option<-function(name, obj){
+    options<-rjd3toolkit::.jd3_env$tramoseats
+    options[[name]]<-obj
+    assign("tramoseats", options, rjd3toolkit::.jd3_env)
+    invisible()
+}
+
+#' Set an option for tramoseats
+#'
+#' @param name Name of the option
+#'
+#' @returns The requested option or NULL if it doesn't exist
+#' @export
+#'
+#' @examples
+#' tramoseats_option("test", "DUMMY")
+#' get_tramoseats_option("test")
+get_tramoseats_option<-function(name){
+    options<-rjd3toolkit::.jd3_env$tramoseats
+    return (options[[name]])
+}
+
+
