e66b4b9dd754cff7439365094487fd9d4e5d17ba
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-06-29 13:10:52 +0000

    update DESCRIPTION with rjd3jars

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,6 @@
 NULL
 
 #' @importFrom rjd3jars check_java_version
-
 #' @importFrom stats is.ts start
 #' @importFrom RProtoBuf read readProtoFiles2
 #' @importFrom rjd3toolkit get_java_version minimal_java_version
@@ -20,12 +19,12 @@ NULL
         rjd3jars::reload_dictionaries()
     }
 
-    assign("providers", list(), rjd3toolkit::.jd3_env)
+    # assign("providers", list(), rjd3toolkit::.jd3_env)
 
     proto.dir <- system.file("proto", package = pkgname)
     RProtoBuf::readProtoFiles2(protoPath = proto.dir)
 
-    assign("tramoseats", list(), rjd3toolkit::.jd3_env)
+    # assign("tramoseats", list(), rjd3toolkit::.jd3_env)
 
     if (is.null(getOption("summary_info"))) {
         options(summary_info = TRUE)
